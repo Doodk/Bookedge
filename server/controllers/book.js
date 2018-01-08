431fe2c1ef546216b339ef13ca250ca86beda6f3
Kevin He <k.he933@gmail.com>
2018-01-07 21:33:06 -0800

    no console.log

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -3,11 +3,9 @@ const google = require('googleapis');
 const books = google.books('v1');
 
 exports.getBook = (req, res) => {
-  console.log('getBook');
   const query = {
     q: req.params.q
   };
-  console.log(query.q);
 
   books.volumes.list(query, (_, result) => {
     res.json(result);
